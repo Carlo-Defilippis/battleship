3ea7fe1ee75e29483a1016d34618570c85145cc4
Damian <damianoverton7011@gmail.com>
2020-10-19 17:29:47 -0400

    Updating index page to select where the boats will be.

--- damian/app/routes.py
+++ damian/app/routes.py
@@ -1,16 +1,22 @@
-from flask import render_template
+from flask import Flask, render_template, request, jsonify, url_for, redirect
 from app import app
 
 @app.route('/')
-@app.route('/index')
 
+@app.route('/index', methods = ['GET', 'POST'])
 def index():
     user = {'username': 'Damian'}
+
+    if request.method == 'POST':
+        board = request.form.get('board')
+        
+        return redirect(url_for('board', board=board))
     return render_template('index.html', title='Home', user=user)
 
 @app.route('/board')
 def board():
-    
+    board = request.args.get('board', None)
+    print(board)
     e_board = [
             ["A","  ","  ","  ","  ","  ","  ","  ","  ","  "," "],
             ["B","  ","  ","  ","  ","  ","  ","  ","  ","  "," "],
@@ -43,3 +49,5 @@ def board():
             e_board=e_board,
             m_board=m_board)
 
+if __name__ == '__main__':
+    app.run(debug=True)
